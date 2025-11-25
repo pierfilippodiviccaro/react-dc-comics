24d13f62212be433f0c7daae6acc954a84f98b41
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-25 20:02:45 +0100

    feat: continuazione footer

--- src/components/Piede.jsx
+++ src/components/Piede.jsx
@@ -1,7 +1,30 @@
 export default function Footer(){
     return(
-<div className="background">
-    miao
+<div className="background flex between no-repat">
+    <div className="">
+        <ul><h3>miao</h3>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>
+        <ul><h3>sksk</h3>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        </ul>
+        <ul><h3></h3>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li></ul>
+    </div>
+    
+    <div>
+        <img src="/img/dc-logo-bg.png" alt="" className="grandezza"/>
+    </div>
 </div>
     )
 }
