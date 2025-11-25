706aa1468e5d67db891f1aa18e778f6fd70be2fe
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-25 17:02:00 +0100

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,24 @@
+
+export default function header(){
+    return(
+        <header>
+            <div className="header">
+                <img src="/img/dc-logo.png" alt="" />
+                <div className="links">
+                   <nav>
+                    <li><a href="">conoscici</a></li>
+                    <li><a href="">andiamo al mare </a></li>
+                    <li><a href="">controlliamo la spazzatura </a></li>
+                    <li><a href="">corallo</a></li>
+                    <li><a href="">miao</a></li>
+                    <li><a href="">proa</a></li>
+                    <li><a href="">cammello</a></li>
+                    <li><a href="">patata</a></li>
+                    <li><a href="">superlativo</a></li>
+                    <li><a href="">croato</a></li>
+                   </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
