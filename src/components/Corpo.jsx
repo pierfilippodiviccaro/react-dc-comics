5241aa4ffb78c5b8acbd3564eeeb49de23de6848
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-11-26 17:40:56 +0100

    feat: continuato

--- src/components/Corpo.jsx
+++ src/components/Corpo.jsx
@@ -4,7 +4,7 @@ return (
 <main>
 <div className="maio blue flex text justify align">
     <nav className="flex row gap2 maio none style top">
-        <li><img src="/img/buy-comics-digital-comics.png" alt="" className="grandezza"/><a href="">miao</a></li>
+        <li className="flex"><img src="/img/buy-comics-digital-comics.png" alt="" className="grandezza"/><a href="">miao</a></li>
         <li><img src="/img/buy-comics-merchandise.png" alt="" className="grandezza" /><a href="">miao</a></li>
         <li><img src="/img/buy-comics-shop-locator.png" alt="" className="grandezza" /><a href="">miao</a></li>
         <li><img src="/img/buy-comics-subscriptions.png" alt="" className="grandezza" /><a href="">miao</a></li>
